fd88e041f674d4d1e03749f5843a99f6ac8b866f
Juan Francisco Gonzalez <juan.ramos.fg1@gmail.com>
2014-04-06 18:40:04 +0100

    added unitTestCase and readme

--- Cortes2DAA_NR/test/UnitTestCase.cpp
+++ Cortes2DAA_NR/test/UnitTestCase.cpp
@@ -1,8 +1,18 @@
-#include"UnitTestCase.h"
+/** ******************************************************
+ * Diseño y analisis de algoritmos                       *
+ * El problema de corte bidimensional sin restricciones  *
+ * es el problema de cortar de una lamina plana          *
+ * rectangular un conjunto de piezas rectangulares más   *
+ * pequeñas, de un tamaño dado, y cada una con un valor, * 
+ * de manera que se maximice el valor de las piezas      *
+ * a cortar.                                             *
+ *                                                       *
+ * @author Juan Francisco González Ramos                 * 
+ * @version 0.2                                          *
+ * @copyright Creative Commons
+ * *******************************************************/
 
-
-//-----------------------------------------------------------------------------
-
+#include "UnitTestCase.h"
 void UnitTestCase::test(void)
 {
   
@@ -11,9 +21,7 @@ void UnitTestCase::test(void)
   fich.close ();
   cortes.cortes (10, 10);
  
-  
-
-        CPPUNIT_ASSERT( cortes.getPos (10, 10)== 80);
+  CPPUNIT_ASSERT( cortes.getPos (10, 10)== 80);
 }
 
  
